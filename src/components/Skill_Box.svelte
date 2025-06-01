b17cc3d997ad810c3d1075720338ae111b23084f
Pitchakorn Thiprangsi <kokejohh@gmail.com>
2025-06-01 22:24:19 +0700

    feat: add skills and use @render

---
+++ src/components/Skill_Box.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    const { children } = $props();
+
+</script>
+
+<div class="flex flex-row flex-wrap space-y-2 space-x-2">
+    {@render children()}
+</div>
--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -1,4 +1,5 @@
 <script>
+    import SkillBox from './Skill_Box.svelte';
     import SkillHover from './Skill_hover.svelte';
 </script>
 
@@ -14,46 +15,60 @@
         <SkillHover icon_class="file-icons:go" text="Go" />
         </div>
     </div>
-     <div class="lg:col-start-4 lg:col-end-7 flex flex-col">
+    <div class="lg:col-start-4 lg:col-end-7 flex flex-col">
         <p>Front-end development</p>
-        <div class="flex flex-row flex-wrap space-y-2 space-x-2">
+        <SkillBox>
             <SkillHover icon_class="devicon-plain:html5" text="HTML5" />
             <SkillHover icon_class="devicon-plain:css3" text="CSS3" />
             <SkillHover icon_class="devicon-plain:javascript" text="JavaScript" />
             <SkillHover icon_class="devicon-plain:typescript" text="TypeScript" />
-            <SkillHover icon_class="devicon-plain:nextjs" text="Next.js" />
-            <SkillHover icon_class="devicon-plain:nuxtjs" text="Nuxt" />
-            <SkillHover icon_class="devicon-plain:svelte" text="Svelte" />
             <SkillHover icon_class="file-icons:tailwind" text="TailwindCSS" />
-        </div>
+        </SkillBox>
      </div>
      <div class="lg:col-start-1 lg:col-end-4 flex flex-col">
         <p>Back-end development</p>
-        <div class="flex flex-row space-x-2">
+        <SkillBox>
             <SkillHover icon_class="devicon-plain:nodejs" text="Node.js" />
-            <SkillHover icon_class="file-icons:sequelize" text="Sequelize" />
-            <SkillHover icon_class="file-icons:prisma" text="Prisma" />
-            <SkillHover icon_class="devicon-plain:mysql-wordmark" text="MySQL" />
             <SkillHover icon_class="devicon-plain:php" text="PHP" />
             <SkillHover icon_class="devicon-plain:salesforce" text="Salesforce" />
-        </div>
+            <SkillHover icon_class="devicon-plain:spring" text="Spring Boot" />
+        </SkillBox>
+    </div>
+     <div class="lg:col-start-4 lg:col-end-7 flex flex-col">
+        <p>Fullstack Frameworks</p>
+        <SkillBox>
+            <SkillHover icon_class="devicon-plain:nextjs" text="Next.js" />
+            <SkillHover icon_class="devicon-plain:nuxtjs" text="Nuxt.js" />
+            <SkillHover icon_class="devicon-plain:svelte" text="SvelteKit" />
+        </SkillBox>
+    </div>
+    <div class="lg:col-start-1 lg:col-end-4 flex flex-col">
+        <p>Database</p>
+        <SkillBox>
+            <div class="flex flex-row flex-wrap space-y-2 space-x-2">
+                <SkillHover icon_class="file-icons:sequelize" text="Sequelize" />
+                <SkillHover icon_class="file-icons:prisma" text="Prisma" />
+                <SkillHover icon_class="devicon-plain:mysql-wordmark" text="MySQL" />
+                <SkillHover icon_class="devicon-plain:postgresql" text="PostgreSQL" />
+            </div>
+        </SkillBox>
     </div>
      <div class="lg:col-start-4 lg:col-end-7 flex flex-col">
         <p>Game Development</p>
-        <div class="flex flex-row space-x-2">
+        <SkillBox>
             <SkillHover icon_class="devicon-plain:csharp" text="C#" />
             <SkillHover icon_class="devicon-plain:unity" text="Unity" />
             <SkillHover icon_class="file-icons:blender" text="Blender" />
-        </div>
+        </SkillBox>
      </div>
-     <div class="lg:col-start-1 lg:col-end-7 flex flex-col">
+     <div class="lg:col-start-1 lg:col-end-4 flex flex-col">
         <p>Tools</p>
-        <div class="flex flex-row space-x-2">
+        <SkillBox>
             <SkillHover icon_class="devicon-plain:linux" text="Linux" />
             <SkillHover icon_class="devicon-plain:neovim" text="Neovim" />
             <SkillHover icon_class="devicon-plain:git" text="Git" />
             <SkillHover icon_class="devicon-plain:postman" text="Postman" />
             <SkillHover icon_class="file-icons:figma" text="Figma" />
-        </div>
+        </SkillBox>
      </div>
 </article>
